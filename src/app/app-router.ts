4996c89a3afbce1f452d4a2f95444bc78a04fce1
Kevin McShea <kevinmcshea@gmail.com>
2018-08-30 16:34:10 -0700

    added pages and added router + default routing

---
+++ src/app/app-router.ts
@@ -0,0 +1,11 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+export const router: Routes = [
+
+    { path: '',  component: LandingPageComponent },
+    { path: 'landing', component: LandingPageComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
